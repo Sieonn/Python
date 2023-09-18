ff8a56c1deb17e7005ce97f08b5697821c7bc2dc
Sieonn <145124127+Sieonn@users.noreply.github.com>
2023-09-18 20:58:18 +0900

    07.py

---
+++ 기초 300제/01. 기초/07.py
@@ -0,0 +1,8 @@
+print() 함수를 사용하여 다음과 같이 출력하세요.
+naver;kakao;sk;samsung
+
+print("naver;kakao;sk;samsung")
+print("naver", "kakao", "samsung", sep=";")
+
+‘sep’ : separation이란 뜻으로 분리할 문자를 지정해 출력한다. 이를 구분자라고 한다.
+’end’ : end옵션을 사용하면 그 뒤의 출력값과 이어서 출력한다.(줄 바꿈X)
