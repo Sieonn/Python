9d540c6be41e9a4fac1d03eaff7ff7814f385569
Sieon <sieon.eve@gmail.com>
2023-09-19 23:41:31 +0900

    yes

---
+++ 기초 300제/08. 반복문/174.py
@@ -0,0 +1,15 @@
+# 아래와 같이 리스트의 데이터를 출력하라. 단, for문과 range문을 사용하라.
+# price_list = [32100, 32150, 32000, 32500]
+# 100 32150
+# 110 32000
+# 120 32500
+T = 100
+price_list = [32100, 32150, 32000, 32500]
+for i in range(1, len(price_list)):
+    T += 10
+    print(T , price_list[i])
+# ---------------------------------------------------------
+
+price_list = [32100, 32150, 32000, 32500]
+for i in range(1, len(price_list)):
+    print(90 + i*10 , price_list[i])
---
+++ 기초 300제/08. 반복문/180.py
@@ -0,0 +1,14 @@
+# 리스트에 5일간의 저가, 고가 정보가 저장돼 있다. 고가와 저가의 차를 변동폭이라고 정의할 때, 
+# low, high 두 개의 리스트를 사용해서 5일간의 변동폭을 volatility 리스트에 저장하라.
+# low_prices  = [100, 200, 400, 800, 1000]
+# high_prices = [150, 300, 430, 880, 1000]
+
+low_prices  = [100, 200, 400, 800, 1000]
+high_prices = [150, 300, 430, 880, 1000]
+
+# 변동폭 = int(high_prices) - int(high_prices)
+volatility = []
+for i in range(5):
+    변동폭 = int(high_prices[i]) - int(low_prices[i])
+    volatility.append(변동폭)
+print(volatility)
